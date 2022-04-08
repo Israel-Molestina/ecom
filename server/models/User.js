10acff29c46665836c73516814f0ba9d7e92da66
Israel  Molestina <israel@loxo-solutions.com>
2022-04-08 12:52:44 -0400

    createded models for order prodcuts user and category

--- server/models/User.js
+++ server/models/User.js
@@ -21,7 +21,18 @@ const userSchema = new Schema({
   admin: {
     type: Boolean,
   },
-  
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+
   createdAt: {
     type: Date,
     default: Date.now,
@@ -33,3 +44,22 @@ const userSchema = new Schema({
     get: (timestamp) => dateFormat(timestamp),
   },
 });
+
+// // set up pre-save middleware to create password
+// userSchema.pre("save", async function (next) {
+//   if (this.isNew || this.isModified("password")) {
+//     const saltRounds = 10;
+//     this.password = await bcrypt.hash(this.password, saltRounds);
+//   }
+
+//   next();
+// });
+
+// compare the incoming password with the hashed password
+// userSchema.methods.isCorrectPassword = async function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
+
+const User = model("User", userSchema);
+
+module.exports = User;
