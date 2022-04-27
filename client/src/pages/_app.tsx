2a1f3e12804bd94669c88a308e2eeb686684bdbc
Israel  Molestina <israel@loxo-solutions.com>
2022-04-27 18:43:53 -0400

    added _document.js page to the client file to help integrate materialui into nextjs

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Head from 'next/head'
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../styles/theme";
+import createEmotionCache from "../lib/createEmotionCache";
+import "../styles/globals.css";
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const App = (props: MyAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return(
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>My Page</title>
+        <meta name='viewport' content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Component {...pageProps}/>
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
+
+
+export default App;
